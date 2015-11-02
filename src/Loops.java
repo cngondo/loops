95fcc915bde12bbcb489d524167ef70e2ef121f3
cngondo <devngondo@gmail.com>
2015-11-02 20:12:34 +0300

    Comments

--- src/Loops.java
+++ src/Loops.java
@@ -4,7 +4,8 @@
 //Exercises for some of the loops in Java
 public class Loops {
     public static void main(String args []){
-//        Invokes the different classes in the project
+        /*Invokes the different classes in the project.
+        * */
         new Number();
         new Dowhile();
         new While();

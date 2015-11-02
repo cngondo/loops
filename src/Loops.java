00e1a63b72e17a882cee3cf4196b5533d079a3db
cngondo <devngondo@gmail.com>
2015-11-02 19:04:49 +0300

    Print out numbers

---
+++ src/Loops.java
@@ -0,0 +1,12 @@
+/**
+ * Created by ngondo on 11/2/15.
+ */
+
+//Excercises for some of the loops in Java
+public class Loops {
+    public static void main(String args []){
+//        Invokes the number class
+        new Number();
+
+    }
+}
---
+++ src/Number.java
@@ -0,0 +1,13 @@
+/**
+ * Created by ngondo on 11/2/15.
+ */
+public class Number {
+    public Number() {
+//        Prints out numbers from 1-100
+        int x = 0;
+
+        for(x=0; x <= 100; x++){
+            System.out.println(x);
+        }
+    }
+}
